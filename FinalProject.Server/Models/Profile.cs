980322c8cb68b7d5a08e10a232cd203d947c660f
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-28 13:31:46 -0600

    Keep Model done

--- FinalProject.Server/Models/Profile.cs
+++ FinalProject.Server/Models/Profile.cs
@@ -1,7 +1,10 @@
 namespace FinalProject.Server.Models
 {
-    public class Account
-    {
-        
-    }
+  public class Profile
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Picture { get; set; }
+  }
 }

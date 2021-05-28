fac5ff56b5b9c522fdddcb02344a433e7c321fc5
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-28 13:37:51 -0600

    Vaultkeep Model Done

--- FinalProject.Server/Models/Vaultkeep.cs
+++ FinalProject.Server/Models/Vaultkeep.cs
@@ -1,7 +1,10 @@
 namespace FinalProject.Server.Models
 {
-    public class Vaultkeep
-    {
-        
-    }
+  public class Vaultkeep
+  {
+    public int Id { get; set; }
+    public string CreatorId { get; set; }
+    public int VaultId { get; set; }
+    public int KeepId { get; set; }
+  }
 }

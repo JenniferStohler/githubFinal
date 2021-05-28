78e1480fcfd213b624812fc95022a620f909df3a
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-28 16:01:43 -0600

    GetProfile in Repository Done

--- FinalProject.Server/Repositories/ProfileRepository.cs
+++ FinalProject.Server/Repositories/ProfileRepository.cs
@@ -1,13 +1,21 @@
 using System;
+using System.Data;
+using Dapper;
 using FinalProject.Server.Models;
 
 namespace FinalProject.Server.Repositories
 {
   public class ProfileRepository
   {
+    private readonly IDbConnection _db;
+    public ProfileRepository(IDbConnection db)
+    {
+      _db = db;
+    }
     internal Profile GetById(string id)
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM accounts WHERE id =@id";
+      return _db.QueryFirstOrDefault<Profile>(sql, new { id });
     }
   }
 }

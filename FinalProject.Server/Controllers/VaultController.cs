cb45996a2891ba5561e87c2121efc167deb4aa07
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-28 20:51:28 -0600

    working on Vaults

--- FinalProject.Server/Controllers/VaultController.cs
+++ FinalProject.Server/Controllers/VaultController.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using FinalProject.Server.Models;
+using FinalProject.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace FinalProject.Server.Controllers
 {
-    public class VaultController
+  [ApiController]
+  [Route("api/[controller]")]
+  public class VaultController : ControllerBase
+  {
+    private readonly VaultService _vs;
+    public VaultController(VaultService vs)
     {
-        
+      _vs = vs;
     }
+    [HttpGet]
+    public ActionResult<List<Vault>> GetAll()
+    {
+      List<Vault> vaults = _vs.GetAll();
+      return Ok(vaults);
+    }
+  }
 }

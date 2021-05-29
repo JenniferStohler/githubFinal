33f8b09da7434adb11d6a3ac5a1ca6307c237d48
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-28 21:47:51 -0600

    Vault Controller Done

--- FinalProject.Server/Controllers/VaultController.cs
+++ FinalProject.Server/Controllers/VaultController.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
 using FinalProject.Server.Models;
 using FinalProject.Server.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Server.Controllers
@@ -17,8 +21,93 @@ namespace FinalProject.Server.Controllers
     [HttpGet]
     public ActionResult<List<Vault>> GetAll()
     {
-      List<Vault> vaults = _vs.GetAll();
-      return Ok(vaults);
+      try
+      {
+        List<Vault> vaults = _vs.GetAll();
+        return Ok(vaults);
+      }
+      catch (Exception e)
+      {
+
+        return BadRequest(e.Message);
+      }
     }
+    [HttpGet("{id}")]
+    public ActionResult<Vault> Get(int id)
+    {
+      try
+      {
+        Vault vault = _vs.Get(id);
+        return Ok(vault);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpGet("{id}/keeps")]
+    public ActionResult<List<Keep>> GetKeeps(int id)
+    {
+      try
+      {
+        List<Keep> keeps = _ks.GetKeeps(id);
+        return Ok(keeps);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<Vault>> Create([FromBody] Vault v)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        v.CreatorId = userInfo.Id;
+        Vault newV = _vs.Create(v);
+        newV.Creator = userInfo;
+        return Ok(newV);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [Authorize]
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Vault>> Update(int id, [FromBody] Vault v)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        v.Id = id;
+        Vault newV = _vs.Update(v, userInfo.Id);
+        newV.Creator = userInfo;
+        return Ok(newV);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [Authorize]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<string>> Delete(int id)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        _vs.Delete(id, userInfo.Id);
+        return Ok("Deleted");
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
   }
 }
--- FinalProject.Server/Services/VaultService.cs
+++ FinalProject.Server/Services/VaultService.cs
@@ -7,4 +7,24 @@ public class VaultService
   {
     throw new NotImplementedException();
   }
+
+  internal Vault Get(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  internal Vault Create(Vault v)
+  {
+    throw new NotImplementedException();
+  }
+
+  internal Vault Update(Vault v, string id)
+  {
+    throw new NotImplementedException();
+  }
+
+  internal void Delete(int id1, string id2)
+  {
+    throw new NotImplementedException();
+  }
 }

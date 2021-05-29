749d71c4cabc8e0dba2a4a4ada3b7c106639e2d6
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-29 13:08:08 -0600

    Keeps Started

--- FinalProject.Server/Controllers/KeepController.cs
+++ FinalProject.Server/Controllers/KeepController.cs
@@ -1,7 +1,38 @@
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using FinalProject.Server.Models;
+using FinalProject.Server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace FinalProject.Server.Controllers
 {
-    public class KeepController
+  [ApiController]
+  [Route("api/[controller")]
+  public class KeepController : ControllerBase
+  {
+    private readonly KeepService _ks;
+    public KeepController(KeepService ks)
     {
-        
+      _ks = ks;
     }
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<KeepService>> Create([FromBody] Keep k)
+    {
+      try
+      {
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        k.CreatorId = userInfo.Id;
+        Keep newK = _ks.Create(k);
+        newK.Creator = userInfo;
+        return Ok(newK);
+      }
+      catch (System.Exception)
+      {
+
+        throw;
+      }
+    }
+  }
 }
--- FinalProject.Server/Services/KeepService.cs
+++ FinalProject.Server/Services/KeepService.cs
@@ -1,7 +1,20 @@
+using System;
+using FinalProject.Server.Models;
+using FinalProject.Server.Repositories;
+
 namespace FinalProject.Server.Services
 {
-    public class KeepService
+  public class KeepService
+  {
+    private readonly KeepRepository _kp;
+    public KeepService(KeepRepository kp)
     {
-        
+      _kp = kp;
     }
+
+    internal Keep Create(Keep k)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }

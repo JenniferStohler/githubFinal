75f7393882514feb90b6e4b0a621572477e2dbf0
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-05-30 17:34:55 -0600

    small page fix

--- FinalProject-Client/src/pages/ProfilePage.vue
+++ FinalProject-Client/src/pages/ProfilePage.vue
@@ -1,38 +1,37 @@
 <template>
-  <template>
-    <div class="about text-center" v-if="state.activeProfile">
-      <div class="col-12">
-        <div class="d-flex">
-          <img class="rounded-circle" :src="state.activeProfile.picture" alt="Profile Image">
-          <div class="d-flex flex-column justify-content-centerr">
-            <h4 class="text-dark">
-              {{ state.activeProfile.name }}
-            </h4>
-          </div>
+  <div class="about text-center" v-if="state.activeProfile">
+    <div class="col-12">
+      <div class="d-flex">
+        <img class="rounded-circle" :src="state.activeProfile.picture" alt="Profile Image">
+        <div class="d-flex flex-column justify-content-centerr">
+          <h4 class="text-dark">
+            {{ state.activeProfile.name }}
+          </h4>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
-  <script>
-    import { computed, reactive, onMounted } from 'vue'
-    import { useRoute } from 'vue-router'
-    import { AppState } from '../AppState'
-    import { profilesService } from '../services/ProfilesService.js'
+<script>
+import { computed, reactive, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { AppState } from '../AppState'
+import { profilesService } from '../services/ProfilesService.js'
 
-    // Gets the Profile
-    export default {
-    setup() {
+// Gets the Profile
+export default {
+  setup() {
     const route = useRoute()
     onMounted(async() => {
-    await profilesService.getProfile(route.params.id)
+      await profilesService.getProfile(route.params.id)
     })
     return reactive({
-    profile: computed(() => AppState.profile)
+      profile: computed(() => AppState.profile)
     })
-    }
-    }
-  </script>
+  }
+}
+</script>
   <style scoped>
   </style>
 </template>

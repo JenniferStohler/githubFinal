31508b5262c4ffcc958c34cdd00ad923ee41f5a4
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-06-05 16:45:02 -0600

    ProfileService Done

---
+++ FinalProject-Client/src/services/ProfilesService.js
@@ -0,0 +1,26 @@
+import { AppState } from '../AppState'
+import { api } from './AxiosService'
+
+class ProfilesService {
+  async getActiveProfile(id) {
+    const res = await api.get('api/profiles/' + id)
+    AppState.activeProfile = res.data
+  }
+
+  async getProfileKeeps(id) {
+    const res = await api.get(`api/profiles/${id}/keeps`)
+    AppState.profileKeeps = res.data
+  }
+
+  async getProfileVaults(id) {
+    const res = await api.get(`api/profiles/${id}/vaults`)
+    AppState.profileVaults = res.data
+    console.log(res.data)
+  }
+
+  async getUserVaults(id) {
+    const res = await api.get('account/vaults')
+    AppState.userVaults = res.data
+  }
+}
+export const profilesService = new ProfilesService()
